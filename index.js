ef292fe7b94bc46e4a0442f9795cad3aeb360f08
Alex Ramos <alex@example.com>
2018-10-31 08:52:59 +0100

    Ejercicio finalizado 8:53

--- index.js
+++ index.js
@@ -9,6 +9,46 @@ function onClick (_event) {
 
   fetch(perretes)
   .then (toJason)
+  .then (appendImgDom)
+};
+  
+function toJason (apiResponse) {
+  return apiResponse.json(); 
+};
+
+function appendImgDom(jsonResponse) {
+  const img = makeImageFrom(jsonResponse.message);
+  document.querySelector(".doggos").appendChild(img);
+}
+  
+function makeImageFrom (dogUrl) {
+  const image = document.createElement("img");
+  image.alt = "perretes bonitos";
+  image.src = dogUrl;
+  return image;
+ };
+
+
+
+
+
+
+
+
+
+
+
+/*
+----------------------------------------------------------------------------------------------------------------
+Solución creada por mí con lo de las funciones del .then
+----------------------------------------------------------------------------------------------------------------
+function onClick (_event) {
+  const perretes = "https://dog.ceo/api/breeds/image/random";
+
+
+  fetch(perretes)
+  .then (toJason)
+  .then (monkey)
   .then (DonkeyKong)
 };
 
@@ -17,9 +57,47 @@ function toJason (imagen) {
   return imagen.json();
 };
 
-function DonkeyKong (json) {
+function monkey (json) {
   const img = document.createElement("img");
   img.alt = "perretes bonitos";
   img.src = json.message;
+  return img;
+};
+
+function DonkeyKong (img) {
   document.querySelector(".doggos").appendChild(img);
 };
+
+*/
+
+
+/*
+
+function onClick (_event) {
+  const perretes = "https://dog.ceo/api/breeds/image/random";
+
+  fetch(perretes)
+  .then (toJason)
+  .then (function (jsonResponse) {
+    const img = monkey(jsonResponse.message);
+    document.querySelector(".doggos").appendChild(img);
+  })
+};
+
+
+function toJason (imagen) {
+  return imagen.json();
+};
+
+function monkey (dogUrl) {
+  const img = document.createElement("img");
+  img.alt = "perretes bonitos";
+  img.src = json.message;
+  return img;
+};
+
+*/
+
+
+
+
